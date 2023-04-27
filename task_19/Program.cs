cb8d71f9393f982420f1b1493666c32f2a4371cd
Alex Lovy <Alex.lovy@gmail.com>
2023-04-28 01:23:21 +0300

    add tasks 19/21/23

---
+++ task_19/Program.cs
@@ -0,0 +1,49 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+
+
+Console.WriteLine("Введите число ");
+
+string n = Console.ReadLine();
+
+double result = Int64.Parse(n);
+
+char[] ch = n.ToCharArray();
+
+int max = Convert.ToInt32((n.Length));
+
+int m = max - 1;
+
+int i = 0;
+
+int control = 0;
+
+while (i <= (max/2))
+   {  
+    control = control + (ch[i] - ch[m]);
+
+     m = m - 1;
+
+         i++;
+         
+    if (control != 0)
+     {
+        i = m;
+     }
+     else {}
+  } 
+   
+if (i != m)
+  {
+    Console.WriteLine($"Число {result} является палидромом ");
+  }
+else 
+{ 
+    Console.WriteLine($"Число {result} не является палидромом ");
+} 
